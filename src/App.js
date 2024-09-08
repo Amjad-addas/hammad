9b4854e9d382090d735f2e2af71a3fdb21d36b92
Amjad-addas <65742033+Amjad-addas@users.noreply.github.com>
2024-09-08 14:50:40 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Hero from './hero/Hero';
+import Navbar from './navbar/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <Hero/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/hero/Hero.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import logo from './logo.svg'
+import style from './hero.module.css'
+function Hero() {
+  return (
+    <div className={style.hero}>
+        <img src={logo} className={style.logo} alt="logo" />
+        <p>parghraph about the website</p>
+        <input className={style.input} type="text" placeholder='Введите код компонента / Write code component' />
+        <button className={style.btn}>Render</button>
+    </div>
+  )
+}
+
+export default Hero
